7845ea003c8c1bc90b4e2ecd31bcabfb5afa3401
Anshul Riyal <anshulriyal00@gmail.com>
2022-07-09 23:23:29 +0530

    devise JWT onfig added
    Ideais to add auth from web and api

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,32 @@
+class Users::SessionsController < Devise::SessionsController
+  respond_to :json
+  def create
+    super
+    respond_to do |format|
+      format.html {
+        @user.save ? (render @user) : (render :new)
+      }
+      format.json {
+        @user.save ? (render :json => {:state => {:code => 0}, :data => @user }) : render (:json => {:state => {:code => 1, :messages => @user.errors.full_messages} })
+      }
+    end
+  private
+
+  def respond_with(resource, _opts = {})
+    render json: { message: 'You are logged in.' }, status: :ok
+  end
+
+  def respond_to_on_destroy
+    log_out_success && return if current_user
+
+    log_out_failure
+  end
+
+  def log_out_success
+    render json: { message: "You are logged out." }, status: :ok
+  end
+
+  def log_out_failure
+    render json: { message: "NO CHANGES."}, status: :unauthorized
+  end
+end
